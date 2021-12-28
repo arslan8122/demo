1457201cd7e604383ba67c8663f97fee35933125
unknown <arslan8122@gmail.com>
2021-12-29 01:36:27 +0500

    first commit

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Image, View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/components/HomeScreen';
@@ -6,13 +7,60 @@ import Favorites from './src/components/Favorites';
 import Updates from './src/components/Updates';
 import You from './src/components/You';
 import Cart from './src/components/Cart';
-
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          headerTitleStyle: {color: 'white'},
+          headerStyle: {
+            backgroundColor: 'rgba(34,36,40,1)',
+          },
+          tabBarStyle: {
+            backgroundColor: 'rgba(34,36,40,1)',
+          },
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = require('./assets/home.png');
+            } else if (route.name === 'Updates') {
+              iconName = require('./assets/bell.png');
+            } else if (route.name === 'You') {
+              iconName = require('./assets/user.png');
+            } else if (route.name === 'Cart') {
+              iconName = require('./assets/cart.png');
+            } else if (route.name === 'Favorites') {
+              iconName = require('./assets/heart.png');
+            }
+            // You can return any component that you like here!
+
+            return (
+              // <MaterialCommunityIcons name="rocket" />
+              <View>
+                <Image
+                  source={iconName}
+                  resizeMode="contain"
+                  style={{
+                    width: 25,
+                    height: 25,
+                    tintColor: focused ? 'white' : 'grey',
+                  }}
+                />
+              </View>
+            );
+          },
+          tabBarActiveTintColor: 'white',
+          tabBarInactiveTintColor: 'gray',
+        })}
+        tabBarOptions={{
+          showLabel: true,
+          showIcon: true,
+
+          keyboardHidesTabBar: true,
+        }}>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Updates" component={Updates} />
         <Tab.Screen name="You" component={You} />
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -9,7 +9,22 @@ const Tab = createMaterialTopTabNavigator();
 
 const HomeScreen = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarIndicatorStyle: {backgroundColor: 'gray', height: 3},
+      }}
+      tabBarOptions={{
+        //other properties
+        labelStyle: {
+          color: 'white',
+        },
+        pressColor: 'gray',
+        //for click (ripple) effect color
+        style: {
+          backgroundColor: 'rgba(34,36,40,1)',
+          activeTintColor: 'white', //color you want to change
+        },
+      }}>
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Reviews" component={Reviews} />
       <Tab.Screen name="About" component={About} />
